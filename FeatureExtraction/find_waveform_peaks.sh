01f253619359be7d6715dde50d81c9806cbbc5b5
StealthxFox <calvinha721@gmail.com>
2018-05-03 07:48:13 +0000

    Eliminated extraneous files and fixed file naming convention

--- FeatureExtraction/find_waveform_peaks.sh
+++ FeatureExtraction/find_waveform_peaks.sh
@@ -10,7 +10,7 @@ if [ "$#" -ne 3 ]; then
 	    exit
     fi
 
-./waveform_peak_find $1 waveform_peaks_output_x.csv waveform_gnuplot_x.csv waveform_peaks_output_y.csv waveform_gnuplot_y.csv $2 $3
+./XCORR $1 waveform_peaks_output_x.csv waveform_gnuplot_x.csv waveform_peaks_output_y.csv waveform_gnuplot_y.csv $2 $3
 gnuplot waveform_gnuplot_script
 sudo cp waveform_plot.png /var/www/html/graphics 
 echo "If no error is reported, view the plot image at http://<Beaglebone IP Address>:8080/graphics/waveform_plot.png"
